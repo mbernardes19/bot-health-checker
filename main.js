c0123ae2e600220ef8385ae245e36ff0481ec2a2
mbernardes19 <bernardes.matheus@outlook.com>
2020-10-09 21:03:43 -0300

    Move db connection method to loop

--- main.js
+++ main.js
@@ -23,34 +23,6 @@ airgram.use(new Auth({
 let connection;
 
 void (async function () {
-    function handleDisconnect() {
-        connection = mysql.createConnection({
-            host: process.env.DB_HOST,
-            database: process.env.DB_DATABASE,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            dateStrings: true
-        })
-                                                     // the old one cannot be reused.
-    
-        connection.connect(function(err) {              // The server is either down
-          if(err) {                                     // or restarting (takes a while sometimes).
-            console.log('error when connecting to db:', err);
-            setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-          }                                     // to avoid a hot loop, and to allow our node script to
-        });                                     // process asynchronous requests in the meantime.
-                                                // If you're also serving http, display a 503 error.
-        connection.on('error', function(err) {
-          if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-            handleDisconnect();                         // lost due to either server restart, or a
-          } else {                                      // connnection idle timeout (the wait_timeout
-            throw err;                                  // server variable configures this)
-          }
-        });
-      }
-    
-      handleDisconnect();
-
     const { response: chats } = await airgram.api.getChats({
       limit: 10,
       offsetChatId: 0,
@@ -106,6 +78,33 @@ bot.command('ww', async ctx => {
 bot.launch()
 
   setInterval(async () => {
+    function handleDisconnect() {
+        connection = mysql.createConnection({
+            host: process.env.DB_HOST,
+            database: process.env.DB_DATABASE,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            dateStrings: true
+        })
+    
+        connection.connect(function(err) {             
+          if(err) {                                     
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 2000); 
+          }                                    
+        });                                     
+                                                
+        connection.on('error', function(err) {
+          if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
+            handleDisconnect();                         
+          } else {                                      
+            throw err;                                  
+          }
+        });
+      }
+    
+      handleDisconnect();
+
       console.log('COMECOU A ENVIAR MENSAGENS')
       try {
         await airgram.api.sendMessage({chatId: 980218936, inputMessageContent: {_: 'inputMessageText', text: {_: 'formattedText', text: 'Oi'} }})
